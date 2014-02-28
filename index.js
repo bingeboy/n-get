8275f4fee185461b1cf6d9daac5059235f80bc07
bingeboy <design@jpmcgarrity.com>
2014-02-28 01:11:07 -0500

    allowed for looser rules with destination

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 #! /usr/bin/env node
-process.cwd()
-var colors = require("colors");
-var request = require("request");
-var fs = require("fs");
-var argv = require('minimist')(process.argv.slice(2));
 
-var reqUrl = process.argv[2];
-var writePath = !process.argv[3] ? process.cwd() : process.cwd() + process.argv[3]
+//modules
+var colors = require("colors")
+    , request = require("request")
+    , fs = require("fs")
+    , path = require("path")
+    , argv = require('minimist')(process.argv.slice(2));
 
+//argv
+var reqUrl = process.argv[2]
+    , writePath = !process.argv[3] ? process.cwd() : process.argv[3]
+    , startPath = process.cwd();
+
+var load = "."
+    , url = reqUrl
+    , filename = url.substring(url.lastIndexOf("/"));
+
+    filename = filename.replace("/","");
+
+//Check for destination
+if(!writePath || writePath === null) writePath = "./";
+var x  = chdir(writePath)
+writeFileTo = filename;
+console.log("writeFileTO POST resolve", writeFileTo);
 console.log("writePath".bold, writePath);
 
 
-var load = ".";
-var url = reqUrl;
-var filename = url.substring(url.lastIndexOf("/"));
 console.log("filename: ", filename);
-console.log("Writing to disk : ".green, writePath+filename);
-writePath = writePath + filename;
 
 //regex
 var protocal = /(^http|https|unix|ssh|ftp)/;
@@ -27,30 +37,34 @@ if(!protocal.test(reqUrl)){
     console.log("No Protocal in File".red, " Using:".bold, reqUrl);
 }
 
-//Check for destination
-if(!writePath || writePath === null) {
-    console.log("ere");
-    writePath = "./";
-}
+
 // HTTP GET Request
 if(reqUrl !== null && reqUrl !== undefined && writePath !== undefined)
 
-console.log("get: ".inverse, reqUrl);
-console.log("writing to: ".inverse, writePath);
+console.log("Request: ".inverse.cyan, reqUrl);
 
 var r = request(reqUrl)
             .on("end", function(){
                 console.log("Stream End")
             })
             .on("data", function(chunk){
-                //shhh quite stream in progress
-                console.log(chunk.length);
+            //    console.log('got %d bytes of data', chunk.length);
             })
             .on("error", function(err){
                 console.log("Error in Stream".red, err.code)
             })
-            .pipe(fs.createWriteStream(writePath))
+            .pipe(fs.createWriteStream(writeFileTo))
             .on("finish", function () {
                 console.log("Pipe Closed".rainbow)
             })
 
+//below this ----------------------------- to modules
+function chdir(dir) {
+    try {
+        process.chdir(dir);
+        console.log('Moving Directory: '.bold + process.cwd());
+    }
+    catch (err) {
+        console.log('chdir: ' + err);
+    }
+}
