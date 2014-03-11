ccc13f7f234ae29223a70cca4c123881cc928e42
bingeboy <design@jpmcgarrity.com>
2014-03-10 22:39:25 -0400

    only error left in stream

--- index.js
+++ index.js
@@ -1,69 +1,68 @@
 #! /usr/bin/env node
 
+console.time("formatUri");
+console.time("totaltime");
+
 //modules
 var colors = require("colors")
     , request = require("request")
     , fs = require("fs")
     , path = require("path")
-    , argv = require('minimist')(process.argv.slice(2));
+    , argv = require('minimist')(process.argv)
+    , chdir = require("./utils/chdir")
+    , uriManager = require("./utils/uriManager");
 
 //argv
-var reqUrl = process.argv[2]
-    , writePath = !process.argv[3] ? process.cwd() : process.argv[3]
-    , startPath = process.cwd();
+var writePath
+    , destination
+    , argvs
+    , startPath
+    , reqUrls = [ ]
+    , startPath;
 
-var load = "."
-    , url = reqUrl
-    , filename = url.substring(url.lastIndexOf("/"));
-
-    filename = filename.replace("/","");
+// print process.argv
+process.argv.forEach(function(val, index, array) {
+    //grab next index value for destination to write
+    if (val === "-d") {
+        destination = array[index+1];
+    }
+    //push values to array for processing
+    else if ( index > 1 && index !== array.indexOf(destination + 1) && index !== array.indexOf(destination)){
+        reqUrls.push(val);
+    }
+});
 
 //Check for destination
 if(!writePath || writePath === null) writePath = "./";
-var x  = chdir(writePath)
-writeFileTo = filename;
+//TODO add chdir support var x  = chdir(writePath);
+writePath = destination;
+writeFileTo = chdir(writePath)
 console.log("writePath".bold, writePath);
+console.log("filename: ", uriManager.file);
 
-
-console.log("filename: ", filename);
-
-//regex
-var protocal = /(^http|https|unix|ssh|ftp)/;
-
-//Check for protocol
-if(!protocal.test(reqUrl)){
-    reqUrl = "http://" + reqUrl;
-    console.log("No Protocal in File".red, " Using:".bold, reqUrl);
-}
-
-
-// HTTP GET Request
-if(reqUrl !== null && reqUrl !== undefined && writePath !== undefined)
-
-console.log("Request: ".inverse.cyan, reqUrl);
-
-var r = request(reqUrl)
-            .on("end", function(){
-                console.log("Stream End")
-            })
-            .on("data", function(chunk){
-            //    console.log('got %d bytes of data', chunk.length);
-            })
-            .on("error", function(err){
-                console.log("Error in Stream".red, err.code)
-            })
-            .pipe(fs.createWriteStream(writeFileTo))
-            .on("finish", function () {
-                console.log("Pipe Closed".rainbow)
-            })
-
-//below this ----------------------------- to modules
-function chdir(dir) {
-    try {
-        process.chdir(dir);
-        console.log('Moving Directory: '.bold + process.cwd());
-    }
-    catch (err) {
-        console.log('chdir: ' + err);
+//process urls
+if(reqUrls) recersivePipe(reqUrls.map(uriManager));
+//download
+function recersivePipe(urls){
+    var popped = urls.pop();
+    var r = request(popped).on("end", function(){
+        console.log("Stream End")
+    })
+    .on("data", function(chunk){
+        console.log('got %d bytes of data', chunk.length);
+    })
+    .on("error", function(err){
+        console.log("Error in Stream".red, err.code)
+    })
+    .pipe(fs.createWriteStream(writeFileTo))
+    .on("finish", function () {
+        console.log("Pipe Closed".rainbow)
+    })
+    if(urls.length === 0){
+        return
+    } else {
+        recersivePipe(urls)
     }
 }
+
+
