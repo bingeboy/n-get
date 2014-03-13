24fcb92b27553b108594e99a6029fd3fd546466a
bingeboy <design@jpmcgarrity.com>
2014-03-13 03:11:24 -0400

    started adding tests

---
+++ test/recursivePipeSpec.js
@@ -0,0 +1,14 @@
+
+var expect = require("chai").expect;
+var recersivePipe = require("../lib/recersivePipe.js");
+
+describe("recersivePipe takes uri and optional destination path and streams the data to ./ or desination", function(){
+
+   describe("#recersivePipe()", function(){
+       it("It Should get an array of URI and stream the data", function(){
+           var urls = ["http://google.com"];
+       });
+       it("Should pop off an array value pass that to steam");
+       it("If length of array is > 0 it should recursively call its self with the curreny arraay no out the value that was popped");
+    });
+});
