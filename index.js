554b2f72d9dc7b512affef62985cc9560ed1a5e0
bingeboy <design@jpmcgarrity.com>
2014-03-24 22:51:07 -0400

    added process hrtime to download and silenced logs

--- index.js
+++ index.js
@@ -20,19 +20,16 @@ var  destination
 process.argv.forEach(function(val, index, array) {
     //grab next index value for destination to write
     if (val === "-d") {
-        console.log("this is indeex", index);
         array.slice(index, index+1);
         return destination = array[index+1];
     }
     //crawl domain and log
     if (val === "-rl") {
         var x = array.slice(index, index+1);
-        console.log(x,"xxxxxxxxxxxxxxxxxxxxxx");
         return spiderDomain = array[index+1];
     }
     //push values to array for processing
     else if ( index > 1) {
-        console.log(val, "val");
         reqUrls.push(val);
     }
 });
@@ -41,21 +38,28 @@ process.argv.forEach(function(val, index, array) {
 if(spiderDomain) {
     console.log("Crawling...".red);
     var spider =  uriManager(spiderDomain);
-    var que = [];
 
     var crawler = Crawler.crawl(spider);
     crawler.interval = 500;
 
-    crawler.on("fetchcomplete",function(queueItem){
-        console.log("Spider Completed fetching resource:".yellow,queueItem.url);
+    crawler.on("fetchcomplete",function(queueItem, responseBuffer, response){
+        console.log("Completed fetching resource:",queueItem.url);
+        console.log("");
+        //requrls.push(queueItem.url)
     });
+
+    crawler.on("queueadd", function(data, response){
+        console.log(response);
+    });
+
 }
 
-console.log(spider, "spider");
+if(spider) console.log(spider, "spider");
+
 //process urls
 if(typeof reqUrls && reqUrls.length > 0){
     var sendDownPipe = reqUrls.map(uriManager);
-    var recersivePipe = require("./lib/recersivePipe")
-    recersivePipe(sendDownPipe)
+    var recersivePipe = require("./lib/recersivePipe");
+    recersivePipe(sendDownPipe);
 }
 
